d2efe6d8e145cfc2d349705c9d7663eae547bc26
Akash K V <akashvinod777@gmail.com>
2022-07-12 14:19:13 +0530

    Project slight update

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
-var express = require('express');
-var app = express();
+var express=require("express")
+var mongoose=require("mongoose")
+var bodyParser=require("body-parser")
 var path = require('path')
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+const app=express()
 
-app.get('/', function(req, res) {
-  res.render(path.join(__dirname+'/views/form'));
-});
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+mongoose.connect("mongodb://localhost:27017/formsDB")
+var db=mongoose.connection
+db.on('error',()=>console.log('error connection'))
+db.once('open',()=>console.log('connected db'))
+app.post('/forms',(req, res)=>{
+    var Name=req.body.Name
+    var Course=req.body.Course
+    var Age=req.body.Age
+    var Email=req.body.Email
 
-app.listen(5000,()=>{
-    console.log('Server is listening on port 5000...');
-});
+    var data={
+        "name":Name,
+        "course":Course,
+        "age":Age,
+        "email":Email
+    }
+    db.collection('student data').insertOne(data, (err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("document inserted successfully")
+    })
+    return res.render(__dirname+'/views/success')
+})
+app.set('view engine', 'ejs')
+
+app.get('/',(req, res)=>{
+    return res.render(path.join(__dirname+'/views/form'))
+}).listen(3000);
+console.log("Listening")
